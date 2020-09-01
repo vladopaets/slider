eef37db9d81891f56a2cc19f13ed257de0e5fe2b
Vlad Opaets <vladopaets.fmi@gmail.com>
2020-09-01 22:48:18 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import Index from './components/App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Index/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
